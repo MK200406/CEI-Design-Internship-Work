82bf4d434ecd21ed891139b4fd6646e30c417941
Manish Khatri <128077728+MK200406@users.noreply.github.com>
2024-07-05 23:25:07 +0530

    Add files via upload

---
+++ prog_2.py
@@ -0,0 +1,7 @@
+# Create variables to store your name, age, and favorite hobby. Print these variables
+
+name = "Manish Khatri"
+age = 20
+fav_hobby = "Exploring New Technologies"
+
+print(f"Hello, I am {name} and my age is {age} and my hobby is {fav_hobby}")
