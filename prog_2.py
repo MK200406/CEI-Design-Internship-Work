2b948dbf464db705fc996a6119ebb5e1ff07c949
Manish Khatri <128077728+MK200406@users.noreply.github.com>
2024-07-05 23:50:08 +0530

    Delete prog_2.py

--- prog_2.py
+++
@@ -1,7 +0,0 @@
-# Create variables to store your name, age, and favorite hobby. Print these variables
-
-name = "Manish Khatri"
-age = 20
-fav_hobby = "Exploring New Technologies"
-
-print(f"Hello, I am {name} and my age is {age} and my hobby is {fav_hobby}")
